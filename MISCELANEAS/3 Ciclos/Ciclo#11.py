e9f20886eea6be2211049ca223073b46fc5652cf
sagrario <sagrariocacaris2004@gmail.com>
2022-11-25 16:08:25 -0500

    7 commit

---
+++ MISCELANEAS/3 Ciclos/Ciclo#11.py
@@ -0,0 +1,7 @@
+'''Solicitar 2 números al usuario e imprimir el cociente y el
+residuo del mayor en el menor sin utilizar la división ni el mod.'''
+
+numero=int(input("ingrese el numero que va a dividir: "))
+numero1=int(input("ingrese el numero: "))
+print("El conciente es: ",numero/numero1)
+print("El residuo es: ",numero % numero1)
